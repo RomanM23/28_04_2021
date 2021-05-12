7aab168e04f3c539f66d5ea327debe1917d1af36
roman <roman112358132@gmail.com>
2021-05-12 21:40:13 +0300

    Added lesson5_v1

---
+++ l05/variables.php
@@ -0,0 +1,12 @@
+<?php
+
+$fruit = 'apple';
+$exoticFruit = $fruit;
+echo $fruit, '<br>', $exoticFruit, '<br>' ;
+
+$fruit = 'pear';
+$exoticFruit = $fruit;
+echo $fruit, '<br>', $exoticFruit, '<hr>' ;
+
+
+
