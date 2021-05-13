5261509a1eba97a167f34e20eaf9a3a5a91b0909
roman <roman112358132@gmail.com>
2021-05-13 21:48:52 +0300

    Added lesson4_v1

---
+++ l04/comments.php
@@ -0,0 +1,23 @@
+<?php
+
+//echo 123;
+
+# echo 123;
+
+/*
+echo 123;
+echo 123;
+*/
+
+
+echo 123/*1212132321*/,2222;
+
+/**
+ * Function for cheking some data
+ * @param string $id Id of user
+ * @return array<int, string> Data of current user
+ */
+function test (string $id): array
+{
+        return [];
+}
---
+++ l04/variables.php
@@ -0,0 +1,19 @@
+<?php
+
+$fruit = 'apple';
+$fruit = 'banana';
+
+
+echo $fruit;
+
+//$1test = 1;
+$te3st5 = 1;
+//$%te^st = 1;
+$_test_rrr = 123;
+echo  $_test_rrr, '<br>' ;
+
+$my_little_pony = 'Shimershire';
+$myLittlePony = 'Rainbow Dash';
+$mylittlepony = 'Horse';
+
+echo $my_little_pony, '<br>', $myLittlePony, '<br>', $mylittlepony;
