75a9387c8ff299dca2cb3bbc84a8dc2a946b4698
roman <roman112358132@gmail.com>
2021-05-31 23:46:39 +0300

    lesson_10_v2____html_forms_V1

--- l10/forms.php
+++ l10/forms.php
@@ -6,22 +6,75 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 </head>
 <body>
-    <form>
-        <div>
-            <label for="username">Name</label>
-            <input type="text" name="username" id="username">
 
-            <label>Gendder</label>
-            <input type="radio" name="gender" value="male" id="gender-male">
-            <label for="gender-male">Male</label>
-            <input type="radio" name="gender" value="male" id="gender-famale">
-            <label for="gender-male">Famale</label>
-       </div>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Top navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+</nav>
 
+<main class="container">
+    <div class="bg-light p-5 rounded">
+        <form>
+            <div class="mb-3">
+                <label for="username"> Name</label>
+                <input type="text" name="username" id="username" class="form-control">
+            </div >
 
-    </form>
+            <div class="mb-3">
+                <label class="form-label">Gender</label>
+                <div class="form-check">
+                    <input type="radio" name="gender" value="male" id="gender-male" class="form-check-input">
+                    <label for="gender-male" class="form-check-label">Male</label>
+                </div>
+                <div class="form-check">
+                    <input type="radio" name="gender" value="famale" id="gender-famale" class="form-check-input">
+                    <label for="gender-famale" class="form-check-label">Female</label>
+                </div>
+            </div>
+
+            <div>
+                <label for="Programing_language" class="form-label">Programing language</label>
+                <select name="Programing_language" class="form-control" id="Programing_language">
+                    <optgroup label="Backend">
+                        <option value="php value="" ">PHP</option>
+                        <option value="python">Python</option>
+                        <option value="java">Java</option>
+                    </optgroup>
+                    <optgroup label="Frontend">
+                        <option value="js">JavaScript</option>
+                        <option value="html">HTML</option>
+                        <option value="css">CSS</option>
+                    </optgroup>
+                </select>
+                <button class="btn btn-success">Send</button>
+        </form>
+</main>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
 </body>
 </html>
-
