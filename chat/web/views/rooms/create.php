1b16433f19a0f68932b011750b993c57ec825fb2
roman <roman112358132@gmail.com>
2021-08-08 14:27:58 +0300

    L28: Added websoket demonstration

---
+++ chat/web/views/rooms/create.php
@@ -0,0 +1,5 @@
+<form method="post">
+    <label for="room-title-input">Room title</label>
+    <input type="text" name="title" id="room-title-input" class="form-control">
+    <button type="submit" class="btn btn-success mt-3">Create</button>
+</form>
